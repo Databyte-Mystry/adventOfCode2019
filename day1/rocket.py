5a0799a4b11487dccb9906314045791b451ba3c3
Databyte-Mystry <jeremy.tirador@gmail.com>
2020-01-08 09:36:06 -0800

    Solved part 1 of day 1

--- day1/rocket.py
+++ day1/rocket.py
@@ -1,1 +1,22 @@
-filein = open('input.txt', 'r')
+'''
+Author: Jeremy T.
+Copyright: Public Use
+'''
+
+'''
+ Main Brains!
+'''
+def main():
+    totalFuel = 0
+    filein = open('day1/input.txt', 'r')
+    for mass in filein:
+        fuel = int(mass) // 3 - 2
+        print('Fuel required: {}'.format(fuel))
+        totalFuel += fuel
+    
+    print('Total fuel: {}'.format(totalFuel))
+    filein.close()
+
+main()
+
+# Tear all the bytes!
