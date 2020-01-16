837c8f352553d58dd955ad641bd77eeba77cd938
Databyte-Mystry <jeremy.tirador@gmail.com>
2020-01-16 11:53:31 -0800

    Finished Day 1, Part 2

--- day1/rocket.py
+++ day1/rocket.py
@@ -10,9 +10,19 @@ def main():
     totalFuel = 0
     filein = open('day1/input.txt', 'r')
     for mass in filein:
+        final_fuel = 0
+
         fuel = int(mass) // 3 - 2
+        final_fuel += fuel
         print('Fuel required: {}'.format(fuel))
-        totalFuel += fuel
+
+        while fuel // 3 - 2 > 0:
+            fuel = fuel // 3-2
+            print('Negative fuel: {}'.format(fuel))
+            final_fuel += fuel
+        
+        print('Final Fuel: {}'.format(final_fuel))
+        totalFuel += final_fuel
     
     print('Total fuel: {}'.format(totalFuel))
     filein.close()
